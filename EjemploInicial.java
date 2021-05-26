f5ee3e4352f1a615a124cb5e0806010723890b07
Ezequiel Hernán Villanueva <ezequiel.villanueva@gmail.com>
2021-05-26 12:05:36 -0300

    Add files via upload

---
+++ EjemploInicial.java
@@ -0,0 +1,59 @@
+/**
+ * @File / Archivo: EjemploInicial.java
+ *
+ * @Date : 26 de Mayo de 2021 / 26 of Mayo of 2021
+ * 
+ * @Author : Ezequiel Hernán Villanueva 
+ *
+ * Este es el ejemplo inicial con comentarios 
+ * This is the initial example with comments.
+ *
+ *
+ *
+ * Some source code files with examples from The Book
+ * "JAVA LA GUÍA TOTAL DEL PROGRAMADOR"
+ * a Compiled and Interpreted by a Java Virtual Machine Programming Language
+ * with the slogan "Write Once, Run Everywhere"
+ * and The Paradigm of Object Oriented Programming Language
+ * for various GNU\Linux based Operative Systems, Microsoft Windows, Mac of Apple and much others.
+ * Thanks and Writted by: Sergio Alejandro Dos Santos @ REDUSERS
+ * 
+ * Edited by Me a little time ago!
+ *
+ * You can Copy / Edit / Change / Distribute
+ * GNU/GPL License Software Version 3.0
+ * 
+ * For Educational Purpose.
+ *
+ */
+
+public class EjemploInicial
+{
+	/**
+	 * No hay otros atributos en esta Clase          EjemploInicial 
+	 * There aren't anothers atributes on this Class
+	 * */
+
+	/**
+	 * No hay otros métodos en esta Clase           EjemploInicial
+	 * There aren't anothers methods on this Class   
+	 * */
+
+	/**
+	 * Método Principal de la Clase    EjemploInicial   que contiene las sentencias que se van a ejecutar
+	 * Principal Method od The Class                    that contain the statements that will be executed
+	 * 
+	 * Similar a la función principal      main(int argc, char * argv[])   del Lenguaje de Programación C
+	 * Similar to the principal function                                   of The C Language Programming
+	 */
+	public static void main(String argv[])
+	{
+		/**
+		 * Salida por Pantalla
+		 * Print on The Display Monitor
+		 * */
+		System.out.println("Casi Siempre HolaMundo es el Ejemplo Inicial");
+		System.out.println("Almost always HolaMundo is the Initial Example");
+	}	
+}
+
