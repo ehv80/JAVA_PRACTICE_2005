b60132a5e493d74c2fffde1b1571180a760c4728
ehv80 <ezequiel.villanueva@gmail.com>
2024-11-16 09:23:03 -0300

    Creación de UsoClaseInteger.java

---
+++ UsoClaseInteger.java
@@ -0,0 +1,22 @@
+public class UsoClaseInteger {
+    Integer c;
+
+    public UsoClaseInteger() {
+        /* Creamos un Integer desde un int */
+        Integer a = new Integer(1);
+        /* Creamos un Integer desde un String */
+        Integer b = new Integer("1");
+        /* Si compareTo devuelve cero quiere decir que los dos Integer son iguales */
+        if (a.compareTo(b) == 0) {
+            /* intValue() devuelve el int con el que está instanciado el Integer */
+            int suma = a.intValue() + b.intValue();
+            c = new Integer(suma);
+        }
+        /* Convertimos el Integer a String usando el método toString() */
+        System.out.prinltn("El resultado es: " + c.toString());
+    }
+
+    public void main(String argv[]) {
+        new UsoClaseInteger();
+    }
+}
