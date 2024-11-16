d708870a4dfeeda2054bed0c5e173f2c7215687a
ehv80 <ezequiel.villanueva@gmail.com>
2024-11-16 09:52:29 -0300

    Modificación de UsoClaseRandom.java

--- UsoClaseRandom.java
+++ UsoClaseRandom.java
@@ -1,27 +1,24 @@
 import java.util.*;
 
 public class UsoClaseRandom {
-    public UsoClaseRandom()
-    {
+    public UsoClaseRandom() {
         Random r = new Random();
         System.out.println("\nFlotantes pseudoaleatorios");
-        for(int i=0; i<5; i++)
-        {
+        for (int i = 0; i < 5; i++) {
             System.out.print(r.nextFloat() + " ");
         }
         System.out.println("\nEnteros pseudoaleatorios");
-        for(int i=0; i<5; i++)
-        {
+        for (int i = 0; i < 5; i++) {
             System.out.print(r.nextInt() + " ");
         }
         System.out.println("\nEnteros pseudoaleatorios entre 0 y 50");
-        for(int i=0; i<5; i++)
-        {
+        for (int i = 0; i < 5; i++) {
             System.out.print(r.nextInt(50) + " ");
         }
-        public static void main(String[] args)
-        {
-            new UsoClaseRandom();
-        }
+
+    }
+
+    public static void main(String[] args) {
+        new UsoClaseRandom();
     }
 }
