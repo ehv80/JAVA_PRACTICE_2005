32a8efdcefc9073abfe89ee8a7cf185c7e275fac
ehv80 <ezequiel.villanueva@gmail.com>
2024-11-23 10:49:53 -0300

    Creación de Figura.java

---
+++ Figura.java
@@ -0,0 +1,9 @@
+/* A continuación declaramos la clase abstracta Figura
+ * con la posición de x e y y con un método abstracto para dibujarla
+ */
+abstract class Figura {
+    long posicionX;
+    long posicionY;
+
+    abstract void dibujar();
+}
