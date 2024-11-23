9f70da3c1afc5675e670c0a8e513e8a624cea89a
ehv80 <ezequiel.villanueva@gmail.com>
2024-11-23 10:19:28 -0300

    Creación de PruebaFinal.java

---
+++ PruebaFinal.java
@@ -0,0 +1,10 @@
+public final class PruebaFinal {
+    /*
+     * El ejemplo es muy sencillo y sirve solamente para ver la sintaxis
+     * Al declarar la clase como Final nadie puede heredar nada de PruebaFinal
+     */
+    /* Constructor por defecto no hago nada */
+    public PruebaFinal() {
+
+    }
+}
