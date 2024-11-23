d10a068823eeef00bbf9ac9c6e90b66dbe4b1ce8
ehv80 <ezequiel.villanueva@gmail.com>
2024-11-23 11:02:03 -0300

    Creación de PruebaAbstract.java

---
+++ PruebaAbstract.java
@@ -0,0 +1,12 @@
+/* Ahora creamos la clase PruebaAbstract para probar las clases */
+public class PruebaAbstract {
+    /* Constructor por defecto no hago nada */
+    public PruebaAbstract() {
+
+    }
+
+    public static void main(String[] args) {
+        Circulo c = new Circulo();
+        c.dibujar();
+    }
+}
