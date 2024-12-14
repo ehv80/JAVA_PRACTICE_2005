f9c346f13b3bfb266666abf1d470f160aa63648f
ehv80 <ezequiel.villanueva@gmail.com>
2024-12-14 09:57:35 -0300

    Modificación de Animal.java extends Thread

--- Animal.java
+++ Animal.java
@@ -1,16 +1,29 @@
 /* Ahora veamos un ejemplo combinando atributos y métodos declarados como Final.
  * Para el ejemplo haremos la clase Perro, que hereda de Animal. Veamos la Clase Animal.
  */
-public class Animal {
+public class Animal extends Thread {
+    /* atributos de clase */
+    String nombre = new String();
     /* Declaro tipo como Final */
     final String tipo;
     /* Declaro cantidadDePatas, como no pongo modificador es public */
     int cantidadDePatas;
+    int prioridad;
 
     /* El Constructor por defecto instancia el objeto con valores por defecto */
     public Animal() {
         this.tipo = "Mamífero";
         this.cantidadDePatas = 0;
+    }
+
+    /* Constructor con parámetros */
+    public Animal(String n, String t, int pt, int pd) {
+        this.nombre = n;
+        this.tipo = t;
+        this.cantidadDePatas = pt;
+        this.prioridad = pd;
+        /* Establece la prioridad del Thread a través del método de la clase padre */
+        super.setPriority(this.prioridad);
     }
 
     /* Declaro el método camina como final */
@@ -20,5 +33,10 @@ public class Animal {
 
     public void corre() {
         System.out.println("Soy un Animal y estoy corriendo... ");
+    }
+
+    /* Sobrecargamos el método run */
+    public void run() {
+        System.out.println("Llegó la " + this.nombre);
     }
 }
