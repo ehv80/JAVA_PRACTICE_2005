429f278c442dabbbd20d89eea60fe3f5a102ed01
ehv80 <ezequiel.villanueva@gmail.com>
2024-12-23 20:49:37 -0300

    Modificación de PruebaStatement.java

--- PruebaStatement.java
+++ PruebaStatement.java
@@ -20,6 +20,7 @@ public class PruebaStatement {
      */
     Connection con;
     Statement stmt;
+    ResultSet rs;
 
     /* Constructor por defecto */
     public PruebaStatement() {
@@ -59,6 +60,29 @@ public class PruebaStatement {
             /*
              * Cerramos la Statement y la conexión a la base de datos InterBase
              */
+            rs = stmt.executeQuery("SELECT * FROM ALUMNOS");
+            /*
+             * Preguntamos si se pudo mover al primer registro del cursor, de ser verdadero
+             * significa que hay registros
+             */
+            if (rs.first()) {
+                System.out.println("Resultado de la consulta SQL por alumnos mayores a 18 años: ");
+                /*
+                 * Leemos el String nombre
+                 */
+                System.out.println(rs.getString("nombre"));
+                /*
+                 * Si se puede mover a la siguiente posición en el cursor significa
+                 * que hay más registros resultados de la consulta SQL
+                 */
+                while (rs.next()) {
+                    System.out.println(rs.getString("nombre"));
+                }
+            }
+            /*
+             * Cierra la Statement y la conexión a la Base de datos InterBase llamada
+             * Escuela
+             */
             stmt.close();
             con.close();
         } catch (SQLException ex) {
@@ -73,3 +97,58 @@ public class PruebaStatement {
         new PruebaStatement();
     }
 }
+/*
+ * LA INTERFAZ ResultSet
+ *
+ * Para consultar los datos de una base de datos usamos la sentencia SELECT de
+ * SQL.
+ * Para esto vamos a utilizar una clase del paquete java.sql, la clase
+ * ResultSet.
+ * Habíamos visto que el método executeQuery() de la clase Statement nos
+ * devolvía
+ * una hoja de resultado. Bien, esta hoja es simplemente el resultado de la
+ * ejecución
+ * del SQL, y de ahora en más la llamaremos cursor.
+ * 
+ * Lo primero que hay que saber es que el cursor tiene tantas posiciones como
+ * filas
+ * tenga el resultado de la consulta SQL, y dos más que se sitúan antes del la
+ * primera fila y
+ * después de la última fila. Para mover el cursor a la primera posición
+ * usaremos el método first(), y
+ * para ir a la última posición, el método last(). Para ir a la posición
+ * siguiente utilizaremos
+ * el método next(), y para volver una posición, el método previous().
+ * Si deseamos movernos a un registro en especial, emplearemos el método
+ * absolute(int numero_fila).
+ * Todos estos métodos devuelven una variable de tipo boolean; si el valor es
+ * true, significa que
+ * pudo hacer el traslado en el cursor.
+ * 
+ * Obtener los datos de un Objeto de tipo ResultSet
+ * Para acceder a los datos del cursor podemos usar getTipo(int numeroCampo)
+ * o getTipo(String nombreCampo). Según el tipo de campo, el método puede ser
+ * getFloat, getString, getDate, etc. Es decir que si tenemos el siguiente SQL:
+ * 
+ * SELECT nombre FROM alumnos WHERE cod_alumno=1;
+ * 
+ * Suponiendo que el ResultSet se llame rs, podemos obtener la información del
+ * campo
+ * nombre a través del método getString() haciendo:
+ * 
+ * rs.getString("nombre");
+ * 
+ * O bien:
+ * 
+ * rs.getString(1);
+ * 
+ * Antes de continuar, aclaremos que es necesario tener en cuenta que el número
+ * de columna
+ * se correspondee con el número de columnas del cursor, por lo tanto, será
+ * la columna que establecimos en el SELECT.
+ * Veamos un ejemplo con la misma base de datos InterBase llamada Escuela usando
+ * la tabla
+ * Alumnos, para dejar un poco más aclarada esta cuestión. En este caso, vamos a
+ * mostrar
+ * por pantalla todos los alumnos mayores a 18 años.
+ */
