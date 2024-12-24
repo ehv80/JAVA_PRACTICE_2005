5bc17627a1310b9ddd0e724f9fb8ff768e19aaa3
ehv80 <ezequiel.villanueva@gmail.com>
2024-12-24 10:18:11 -0300

    Modificación de PruebaStatement.java

--- PruebaStatement.java
+++ PruebaStatement.java
@@ -152,3 +152,209 @@ public class PruebaStatement {
  * mostrar
  * por pantalla todos los alumnos mayores a 18 años.
  */
+/*
+ * PROCESO POR LOTES (BATCH)
+ *
+ * En algunos casos vamos a necesitar hacer una serie de actualizaciones en
+ * conjunto, es decir, ejecutar un lote de consultas SQL; para esto se usa
+ * el método executeBatch() de la clase Statement.
+ * Supongamos que, volviendo al ejemplo de la base de datos Escuela,
+ * necesitamos actualizar dos notas y el promedio entre ellas. En este caso
+ * tendremos dos sentencias de SQL para actualizar las notas,
+ * y una tercera para actualizar el promedio
+ * entre ambas notas. Una vez establecida la conexión
+ * y creado el Statement, el código sería el siguiente:
+ * 
+ * stmt.addBatch("UPDATE NOTAS SET PRIMER_NOTA=6 WHERE COD_ALUMNO=1");
+ * stmt.addBatch("UPDATE NOTAS SET SEGUNDA_NOTA=8 WHERE COD_ALUMNO=1");
+ * stmt.
+ * addBatch("UPDATE NOTAS SET PROMEDIO=(PRIMER_NOTA+SEGUNDA_NOTA)/2 WHERE COD_ALUMNO=1"
+ * );
+ * int[] vFilasAfectadas = stmt.executeBatch();
+ * 
+ * Como vemos, debemos ir agregando las sentencias de SQL a través del método
+ * addBatch().
+ * Una vez que terminamos de agregar las sentencias, las ejecutamos con el
+ * método executeBatch(). Este método nos devuelve un arreglo de enteros,
+ * cada uno de los cuáles se corresponde con la cantidad de filas afectadas
+ * por cada SQL. En nuestro caso, el arreglo tendrá tres posiciones,
+ * este será [1, 1, 1], o sea que cada SQL habrá afectado a una sola fila
+ * de la tabla notas.
+ * Si sumamos el vector, el resultado es la cantidad de filas afectadas
+ * por el proceso por lotes completo.
+ * 
+ * Transacciones
+ * En el caso anterior, hay que analizar que pasaría si en medio de
+ * la ejecución del proceso por lotes sucediera algo extraordinario,
+ * es decir, un corte de luz, se cayera el motor de base de datos,
+ * se cortara la ejecución del programa, etc. Lo más probable sería
+ * que la actualización quedara a medio hacer. En este ejemplo, los
+ * daños no son tan graves, pero ¿qué pasaría si en el sistema
+ * de un banco estuviéramos procesando una transacción bancaria
+ * entre dos cuentas? Supongamos ahora que queremos transferir
+ * $5000 de la cuenta 5 a la cuenta 15, y nuestro proceso se corta
+ * en el medio.
+ * 
+ * stmt.addBatch("UPDATE CUENTAS SET EFECTIVO=EFECTIVO-5000 WHERE COD_CUENTA=5"
+ * );
+ * stmt.addBatch("UPDATE CUENTAS SET EFECTIVO=EFECTIVO+5000 WHERE COD_CUENTA=15"
+ * );
+ * int[] vFilasAfectadas = stmt.executeBatch();
+ * 
+ * Si el proceso se corta en la mitad, habiendo debitado $5000 de la cuenta 5,
+ * nunca los acreditará en la cuenta 15, es decir que los $5000 no están en
+ * ningún lugar. Éste es un problema que se produce cuando las bases de datos
+ * son "No Transaccionales"; en otras palabras, ejecutan las sentencias SQL
+ * una por una y van reflejando el resultado sobre la base de datos.
+ * Para evitar esto debemos configurar la base de datos como "Transaccional",
+ * es decir, que ejecute un conjunto de sentencias SQL y refleje el resultado
+ * sólo al final de la transacción (Figura 6). Antes de empezar con un ejemplo,
+ * debemos tener en cuenta que la base de datos no sabe cuándo comienza una
+ * transacción. Para indicarle a la base de datos lo que queremos que ejecute
+ * como una transacción, debemos encerrar el bloque de sentencias SQL entre
+ * un BEGIN y un COMMIT.
+ * 
+ * No Transacciona ___________________________ Transacciona
+ * Sentencia SQL 1 ___________________________ BEGIN
+ * Sentencia SQL 2 ___________________________ Sentencia SQL 1
+ * Sentencia SQL 3 ___________________________ Sentencia SQL 2
+ * . _________________________________________ Sentencia SQL 3
+ * Error _____________________________________ Error
+ * Sentencia SQL 4 ___________________________ Sentencia SQL 4
+ * . _________________________________________ COMMIT
+ * Las sentencias SQL ________________________ Los SQL no será permanentes
+ * 1, 2, 3 son permanentes ___________________ si no llegó al COMMIT.
+ * 
+ * Figura 6. En una base de datos no transaccional, cuando se provoca
+ * un error los SQL ejecutados son permanentes; en una base de datos
+ * transaccional se "deshacen" hasta el último BEGIN, lo que se conoce
+ * como rollback.
+ * 
+ * Entonces, para configurar la base de datos como "Transaccional",
+ * lo que tenemos que hacer es pedirle que no encierre automáticamente
+ * a cada sentencia SQL entre un BEGIN y un COMMIT, ya que en ese caso
+ * seguiría siendo "No Transaccional". Para pedirle esto a la base de datos
+ * usamos el método setAutoCommit() de la clase Connection.
+ * Entonces volviendo al ejemplo de la transacción bancaria, el código
+ * sería:
+ * 
+ * //Ponemos a la base de datos como "Transaccional"
+ * con.setAutoCommit(false);
+ * //Enviamos el BEGIN para comenzar la transacción
+ * stmt.addBatch("BEGIN");
+ * stmt.addBatch("UPDATE CUENTAS SET EFECTIVO=EFECTIVO-5000 WHERE COD_CUENTA=5"
+ * );
+ * stmt.addBatch("UPDATE CUENTAS SET EFECTIVO=EFECTIVO+5000 WHERE COD_CUENTA=15"
+ * );
+ * //Enviamos el COMMIT para terminar la transacción
+ * stmt.addBatch("COMMIT");
+ * int[] vFilasAfectadas = stmt.executeBatch();
+ * 
+ * Ahora sí, nos aseguramos de que si la transacción no se ejecutó completa,
+ * abortamos todo lo que había hecho. Esto se conoce como rollback
+ * (volver atrás). En algunos casos podemos usar el rollback para cancelar
+ * una transacción si no se cumple alguna condición. Por ejemplo, en un
+ * sistema de facturación grabamos la factura y deseamos que termine
+ * si se imprimió la factura; de lo contrario, que aborte. Como vemos,
+ * en este caso no es por error o algo extraordinario, y sin embargo,
+ * necesitamos hacer un rollback.
+ * 
+ * Problemas de las bases de datos transaccionales
+ * Una vez que configuramos la base de datos transaccional, debemos
+ * saber que estará operando de forma concurrente, es decir, antes
+ * atendía una sentencia SQL por vez, pero ahora, un conjunto de
+ * sentencias SQL puede ser atendido como una transacción, o
+ * sea que la base de datos está trabajando de forma concurrente.
+ * El conflicto surge cuando dos transacciones están trabajando
+ * en conjunto sobre la base de datos. El problema que se presenta
+ * es conocido como "dirty read" o lectura sucia.
+ * 
+ * Supongamos el siguiente caso. En un banco hay una tabla llamada Cuentas
+ * que mantiene las cuentas de todos sus clientes; esta tiene los campos
+ * código de cuenta y saldo, e inicialmente posee la información
+ * de dos cuentas (Figura 7).
+ * 
+ * Tabla Cuentas
+ * COD_CUENTA _______ SALDO
+ * 1 ________________ 1.5000,00
+ * 2 ________________ 600,00
+ * Figura 7. Tabla Cuentas con la información inicial.
+ * 
+ * Continuando con el ejemplo, la persona cuya cuenta en un banco es la
+ * número 2 ordena un pago de 100 pesos desde su cuenta a la cuenta número 1.
+ * lo que genera la transacción A:
+ * 
+ * BEGIN
+ * UPDATE CUENTAS SET SALDO=SALDO+100 WHERE COD_CUENTA=1;
+ * UPDATE CUENTAS SET SALDO=SALDO-100 WHERE COD_CUENTA=2;
+ * COMMIT
+ * 
+ * Al mismo tiempo, la persona cuya cuenta es la número 2
+ * está retirando 80 pesos en un cajero automático, lo que
+ * generará la transacción B:
+ * 
+ * BEGIN
+ * UPDATE CUENTAS SET SALDO=SALDO-80 WHERE COD_CUENTA=2
+ * COMMIT
+ * 
+ * Si todo se ejecuta correctamente, la información
+ * final de la tabla CUENTAS debería quedar como se ve
+ * en la Figura 8:
+ * 
+ * Tabla Cuentas
+ * COD_CUENTA _______ SALDO
+ * 1 ________________ 1.6000,00
+ * 2 ________________ 420,00
+ * Figura 8. Tabla CUENTAS con la información final,
+ * si todo se ejecutó correctamente.
+ * 
+ * Pero ya dijimos que las transacciones son concurrentes.
+ * ¿Qué pasa si entra la transacción A y se ejecuta el primer
+ * SQL, luego entra la transacción B y ejecuta su SQL,
+ * luego la transacción A ejecuta el último SQL y el COMMIT,
+ * y a continuación la transacción B ejecuta el COMMIT? (Figura 9)
+ * 
+ * Transacción A: BEGIN
+ * Transacción A: UPDATE CUENTAS SET SALDO=SALDO+100 WHERE COD_CUENTA=1;
+ * Transacción B: UPDATE CUENTAS SET SALDO=SALDO-80 WHERE COD_CUENTA=2;
+ * Transacción A: UPDATE CUENTAS SET SALDO=SALDO-100 WHERE COD_CUENTA=2;
+ * Transacción A: COMMIT
+ * Transacción B: COMMIT
+ * Figura 9. Transacciones concurrentes
+ * 
+ * En este caso, el primer SQL de la transacción A hace SALDO=SALDO+100,
+ * es decir que lee 1.500,00 y le suma 100, por lo que quedan 1.600,00
+ * en la cuenta 1; pero todavía no se escribe en la base de datos,
+ * ya que aún no ejecutó el COMMIT. Ahora viene el SQL de la transacción B,
+ * que hace SALDO=SALDO-80; en esta caso lee 600,00 y le resta 80,00
+ * por lo que quedan 520,00 en la cuenta 2. A continuación la transacción A
+ * ejecuta un SQL sobre la cuenta 2, y resta 100, pero este SQL no trae
+ * ningún problema aparejado lee 600 y le resta 100, ya que aún no se ejecutó
+ * el COMMIT. Finalmente, la transacción A ejecuta su COMMIT dejando
+ * en 1600,00 en el SALDO de la cuenta 1, pero la transacción B
+ * también ejecuta su COMMIT dejando 500 en la cuenta 2, por lo que
+ * finalmente la tabla CUENTAS queda como se muestra en la Figura 9.
+ * 
+ * Tabla Cuentas
+ * COD_CUENTA _______ SALDO
+ * 1 ________________ 1.6000,00
+ * 2 ________________ 500,00
+ * Figura 9. Tabla CUENTAS con la información final,
+ * cuando hay transacciones A y B concurrentes con una lectura sucia.
+ * 
+ * Se puede evitar que un valor sea leído antes de un COMMIT con los niveles
+ * de aislamiento, a través del método setTransactionIsolation()
+ * del objeto de la clase Connection:
+ * 
+ * con.setTransactionIsolation(TRANSACTION_READ_UNCOMMITED);
+ * 
+ * En JDBC contamos con cinco niveles de aislamiento.
+ * El nivel más bajo es para que no soporte transacciones, y el más alto,
+ * para que, mientras una transacción esté abierta, ninguna otra transacción
+ * pueda realizar cambios en los datos leídos por esa transacción.
+ * El nivel más alto es más lento en la ejecución de la aplicación,
+ * ya que se incrementan los bloqueos y se disminuye la concurrencia
+ * de los usuarios. Pero a pesar de tener el método el nivel de
+ * aislamiento que pueda soportarse, depende de las posibilidades
+ * de la base de datos que estemos usando
+ */
